7bb05d5215db5098456d4b14187347079caab729
wlkIV <kanndide@gmail.com>
2023-03-03 15:39:18 -0500

    created app component. added pets.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Luna",
+                breed: "Belgian Shepard"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Pepper",
+                breed: "Tabby"
+            }),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Ein",
+                breed: "Corgi"
+            }),
+        ]
+    )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
